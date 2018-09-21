3c425eede1a3f8ea541d9cb59469e7c6089688ac
Sander Dekkers <sander.dekkers@port-it.nl>
2018-09-21 20:15:48 +0200

    autodiscovery anyway

--- config/app.php
+++ config/app.php
@@ -159,9 +159,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Barryvdh\Debugbar\LaravelDebugbar::class,
-
     ],
 
     /*
